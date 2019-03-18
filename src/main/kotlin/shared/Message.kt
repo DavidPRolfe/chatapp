2f74ac2d92371485819982716ee1b70f709f9e10
DavidPRolfe <david.p.rolfe@gmail.com>
2019-03-18 10:20:59 -0500

    Updated server

--- src/main/kotlin/shared/Message.kt
+++ src/main/kotlin/shared/Message.kt
@@ -1,8 +1,49 @@
 package shared
 
+import java.io.InputStream
+import java.nio.ByteBuffer
+import java.nio.ByteOrder
+
 const val PORT = 35888
 const val MAX_CHAR = Integer.MAX_VALUE //2147483647
 
 data class Message(val headers: Headers, val body: String)
 
 data class Headers(val username: String)
+
+fun decodeMessage(input: InputStream): Message? {
+    // Reads header size
+    val headerSize = ByteArray(4)
+    if (input.read(headerSize) != 4) {
+        println(headerSize.toInt())
+        println("Didn't read header size")
+        return null
+    }
+
+    // Reads message size
+    val messageSize = ByteArray(4)
+    if (input.read(messageSize) != 4) {
+        println("Didn't read message size")
+        return null
+    }
+
+    // Read header
+    val header = ByteArray(headerSize.toInt())
+    if (input.read(header) != headerSize.toInt()) {
+        println("Didn't read header")
+        return null
+    }
+
+    // Read message
+    val message = ByteArray(messageSize.toInt())
+    if (input.read(message) != messageSize.toInt()) {
+
+        println("Didn't read message")
+        return null
+    }
+
+    return Message(Headers(String(header)), String(message))
+}
+
+
+fun ByteArray.toInt(): Int = ByteBuffer.wrap(this).order(ByteOrder.BIG_ENDIAN).int
